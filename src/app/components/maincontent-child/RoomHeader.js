9cdaa56f3d8c4ccabe367617fe49d730f18ad542
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 17:08:59 +0530

    added chat room header

---
+++ src/app/components/maincontent-child/RoomHeader.js
@@ -0,0 +1,31 @@
+import FeatherIcon from 'feather-icons-react';
+import React from 'react';
+
+function RoomHeader(props) {
+	const handleClick = () => {
+		alert('Some Basic Information');
+	};
+
+	return (
+		<>
+			<nav className='navbar is-primary px-5' style={{ zIndex: 1 }}>
+				<div id='innerMenu' className='navbar-menu'>
+					<div className='navbar-start'>
+						<div className='navbar-item'>
+							<FeatherIcon icon='star' fill={props.isFavorite ? 'white' : ''} size='18' className='mr-3' />
+							{props.RoomName !== undefined ? props.RoomName : 'Select Room'}
+						</div>
+					</div>
+					<div className='navbar-end'>
+						<div className='navbar-item' onClick={handleClick}>
+							Info
+							<FeatherIcon icon='info' size='18' className='ml-3' />
+						</div>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default RoomHeader;
