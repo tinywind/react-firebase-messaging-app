c086c56d333a2febf277ee22710f244f00057971
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 18:12:52 +0530

    added sidebar container for rooms

---
+++ src/app/components/sidebar-child/SideBarRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loading from '../../components/common/Loading';
+
+function SideBarRoom() {
+	return (
+		<ul>
+			<Loading />
+		</ul>
+	);
+}
+
+export default SideBarRoom;
