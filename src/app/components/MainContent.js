d060b90a8952d104f927c7ad068f0f29d05f0d52
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 11:19:20 +0530

    added core layout components

---
+++ src/app/components/MainContent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MainContent.scss';
+
+function MainContent() {
+	return <div>Application Main Content Goes here.. some rooms will be here</div>;
+}
+
+export default MainContent;
---
+++ src/app/components/SideBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SideBar.scss';
+
+function SideBar() {
+	return <div>Some Navigation Gimmick and some dynamic links will be added here</div>;
+}
+
+export default SideBar;
