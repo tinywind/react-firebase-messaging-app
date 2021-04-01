b2dbd3d78d049ca4bb5f2ce05e1adf8b5a64d095
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 11:45:08 +0530

    added some side bar navigation links

--- src/app/components/SideBar.js
+++ src/app/components/SideBar.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './SideBar.scss';
 
 function SideBar() {
-	return <div>Some Navigation Gimmick and some dynamic links will be added here</div>;
+	return (
+		<div className='is-max-height side-bar-custom pr-0 mr-0'>
+			<div className='column is-sidebar-menu is-flat'>
+				<aside className='menu'>
+					<div className='is-divider' data-content='Services'></div>
+					<h4 class='subtitle is-4'>Anonymous user!</h4>
+
+					<div className='is-divider' data-content='Rooms'></div>
+					<h6 class='subtitle is-6'>Room 1</h6>
+					<h6 class='subtitle is-6'>Room 2</h6>
+				</aside>
+			</div>
+		</div>
+	);
 }
 
 export default SideBar;
