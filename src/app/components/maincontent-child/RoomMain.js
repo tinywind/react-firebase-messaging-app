5e727f35f58c25bda15f99f716d194299a67fb22
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 17:10:36 +0530

    added chat message main container

---
+++ src/app/components/maincontent-child/RoomMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loading from './../common/Loading';
+import './RoomMain.scss';
+
+function RoomMain() {
+	return (
+		<div>
+			<Loading />
+		</div>
+	);
+}
+
+export default RoomMain;
