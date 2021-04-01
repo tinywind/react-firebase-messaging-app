5f439a2ed8230018e707f3811ba7de50317cf14d
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 18:45:54 +0530

    implemeted UI for rooms listing under sidebar

--- src/app/components/sidebar-child/SideBarRoom.js
+++ src/app/components/sidebar-child/SideBarRoom.js
@@ -1,11 +1,50 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+// import Loading from '../../components/common/Loading';
+import FeatherIcon from 'feather-icons-react';
 import React from 'react';
-import Loading from '../../components/common/Loading';
 
 function SideBarRoom() {
+	const roomDetails = ['General Room', 'Special Room', 'VIP Room', 'Extra Room', 'Private Room'];
+
+	const addRoomHandler = () => {
+		const roomName = prompt('Enter the room name!');
+		if (roomName) {
+			roomDetails.push(roomName);
+			alert(roomName + ' added successfully!');
+		}
+	};
+
+	const selectRoomHandler = (roomName) => {
+		alert(roomName + ' selected successfully!');
+	};
+
+	const renderRooms = (rooms) => {
+		let roomMarkup = '';
+		roomMarkup = rooms.map((r) => (
+			<li>
+				<a className='column is-narrow' onClick={() => selectRoomHandler(r)}>
+					{r}
+				</a>
+			</li>
+		));
+
+		return <ul>{roomMarkup}</ul>;
+	};
+
 	return (
-		<ul>
-			<Loading />
-		</ul>
+		<>
+			<div className='prima-links columns' onClick={addRoomHandler}>
+				<a className='column is-10'>Add Rooms</a>
+				<div className='column is-narrow'>
+					<FeatherIcon icon='plus-circle' size='24' />
+				</div>
+			</div>
+			<div className='is-scroll'>
+				<ul className='menu-list'>
+					<li>{renderRooms(roomDetails)}</li>
+				</ul>
+			</div>
+		</>
 	);
 }
 
