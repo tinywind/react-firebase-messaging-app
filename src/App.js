f0b298a7a29122ea23364afb98abef4aa6321bad
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-01 10:12:23 +0530

    minor structure changes made for the UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+import './App.scss';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-			</header>
+			<header className='App-header'>Welcome tp BizWiz</header>
 		</div>
 	);
 }
---
+++ src/app/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './app/images/logo.svg';
+
+function Login() {
+	return (
+		<div>
+			<img src={logo} className='App-logo' alt='logo' />
+		</div>
+	);
+}
+
+export default Login;
