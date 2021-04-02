afbdda1178a225a9144e985e5c48ff254095d332
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-02 10:23:32 +0530

    implemented add room functionality

--- src/app/components/sidebar-child/SideBarRoom.js
+++ src/app/components/sidebar-child/SideBarRoom.js
@@ -2,15 +2,19 @@
 // import Loading from '../../components/common/Loading';
 import FeatherIcon from 'feather-icons-react';
 import React from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { addToCollection, db } from '../../../firebase';
+import './SideBarRoom.scss';
 
 function SideBarRoom() {
-	const roomDetails = ['General Room', 'Special Room', 'VIP Room', 'Extra Room', 'Private Room'];
+	const [roomDetails, loading, error] = useCollection(db.collection('rooms'));
 
 	const addRoomHandler = () => {
 		const roomName = prompt('Enter the room name!');
 		if (roomName) {
-			roomDetails.push(roomName);
-			alert(roomName + ' added successfully!');
+			addToCollection('rooms', { name: roomName }).then((result) => {
+				alert(roomName + ' added successfully!');
+			});
 		}
 	};
 
@@ -20,11 +24,16 @@ function SideBarRoom() {
 
 	const renderRooms = (rooms) => {
 		let roomMarkup = '';
-		roomMarkup = rooms.map((r) => (
-			<li>
-				<a className='column is-narrow' onClick={() => selectRoomHandler(r)}>
-					{r}
-				</a>
+		roomMarkup = rooms?.docs?.map((doc) => (
+			<li key={doc.id}>
+				<div className='links columns'>
+					<a className='column is-narrow mb-2'>
+						<FeatherIcon icon='message-square' size='18' />
+					</a>
+					<a className='column is-narrow mb-1' onClick={() => selectRoomHandler(doc.id)}>
+						{doc.data().name}
+					</a>
+				</div>
 			</li>
 		));
 
