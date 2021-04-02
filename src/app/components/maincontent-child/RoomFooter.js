c1f5d67f27b50c0eaa45818d70cb60c00545176a
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-04-02 22:38:03 +0530

    implemented emoji control for input message field

--- src/app/components/maincontent-child/RoomFooter.js
+++ src/app/components/maincontent-child/RoomFooter.js
@@ -1,6 +1,8 @@
+import { EmojiData, Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 import FeatherIcon from 'feather-icons-react';
 import firebase from 'firebase';
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../../../firebase';
 import Loading from '../../components/common/Loading';
@@ -9,6 +11,16 @@ import './RoomFooter.scss';
 function RoomFooter(props) {
 	const sendRef = React.useRef();
 	const [user] = useAuthState(auth);
+
+	const [showPicker, setShowPicker] = useState(false);
+
+	const addEmoji = (emoji: EmojiData) => {
+		if ('native' in emoji) {
+			sendRef.current.value += emoji.native;
+			setShowPicker(false);
+			sendRef.current.focus();
+		}
+	};
 
 	const sendMessage = (e) => {
 		e.preventDefault();
@@ -37,16 +49,24 @@ function RoomFooter(props) {
 			<div className='column is-centered has-icons-left'>
 				<div className='control has-icons-left has-icons-right mx-3 mb-3'>
 					<form onSubmit={sendMessage}>
+						<span className='icon is-left '>
+							<FeatherIcon icon='send' size='24' />
+						</span>
 						<input className='input is-primary' type='text' placeholder={`Enter your message...`} ref={sendRef} />
 						<span className='icon is-small is-right'>
 							<FeatherIcon
 								className='fav-icon'
-								icon='send'
+								icon='smile'
 								size='24'
 								onClick={() => {
-									sendMessage();
+									setShowPicker(true);
 								}}
 							/>
+							{showPicker && (
+								<div style={{ position: 'absolute', bottom: 10, right: 10 }}>
+									<Picker emoji='' title='' native={true} onSelect={addEmoji} />
+								</div>
+							)}
 						</span>
 					</form>
 				</div>
